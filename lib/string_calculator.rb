747ecc30d1a4f9882d097b4bc50ea9213ec2d0be
vishwas <vishwasdev01@gmail.com>
2024-04-18 17:34:57 +0530

    Handled case when two numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,6 +3,8 @@ module StringCalculator
   def self.add(string)
     return 0 if string.empty?
 
-    string.to_i
+    return string.to_i if string.length.eql?(1)
+
+    string.split('').map(&:to_i).sum
   end
 end
--- spec/string_calcultor_spec.rb
+++ spec/string_calcultor_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe StringCalculator, "#add" do
   it "returns the number itself for one number" do
     expect(described_class.add("1")).to eql(1)
   end
+
+  it "returns the sum of the numbers for two numbers" do
+    expect(described_class.add("1,2")).to eql(3)
+  end
 end
