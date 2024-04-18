d4a92459ad6bd0c78255d3c726c775aafb2d08cb
vishwas <vishwasdev01@gmail.com>
2024-04-18 18:24:19 +0530

    refactor code

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,15 +1,12 @@
 module StringCalculator
 
   def self.add(string)
-    return 0 if string.empty?
-    return string.to_i if string.length.eql?(1)
-
     numbers = string.scan(/-?\d+/).map(&:to_i).select { |s| s < 1001 }
 
     if numbers.any? { |i| i < 0 }
       raise 'Negatives not allowed'
     end
-    
+
     numbers.sum
   end
 end
