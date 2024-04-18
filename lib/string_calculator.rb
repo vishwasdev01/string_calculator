65ecbd86a6cc4d923899f58974a4fcb27e283690
vishwas <vishwasdev01@gmail.com>
2024-04-18 17:30:03 +0530

    Handle case for one number

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,5 +2,7 @@ module StringCalculator
 
   def self.add(string)
     return 0 if string.empty?
+
+    string.to_i
   end
 end
--- spec/string_calcultor_spec.rb
+++ spec/string_calcultor_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe StringCalculator, "#add" do
   it "returns 0 for empty string" do
     expect(described_class.add("")).to eql(0)
   end
+
+  it "returns the number itself for one number" do
+    expect(described_class.add("1")).to eql(1)
+  end
 end
